1ade37a8bfd82ffe95080149c4961946069aa2cb
Kviatkovskii <sergeikviatkovskii@gmail.com>
2016-08-18 20:54:25 +0300

    Update settings.py, procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn airsbot.wsgi --log-file -
--- airsbot/airsbot/settings.py
+++ airsbot/airsbot/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -32,7 +33,7 @@ DATABASES = {
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['airbot-kviatkovskii.herokuapp.com']
 
 
 # Application definition
@@ -45,7 +46,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'fb_airsbot',
-    'sslserver',
 ]
 
 MIDDLEWARE = [
@@ -136,8 +136,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+SECURE_PROXY_SSL_HEADER = (‘HTTP_X_FORWARDED_PROTO’, ‘https’)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
-STATIC_URL = '/static/'
