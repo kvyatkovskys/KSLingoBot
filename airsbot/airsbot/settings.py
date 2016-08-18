386b3bbeab4654eeb701d9240e1f313676cd664f
Kviatkovskii <sergeikviatkovskii@gmail.com>
2016-08-18 21:10:55 +0300

    Update settings.py

--- airsbot/airsbot/settings.py
+++ airsbot/airsbot/settings.py
@@ -96,12 +96,12 @@ STATICFILES_DIRS = (
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
